152cb6d3d7861f09859c0642b8e5bd9eb3ace4b6
DESKTOP-37Q3L3S\Indra Prayoga S <indraprayogas21@gmail.com>
2022-02-20 14:56:24 +0700

    first

---
+++ rest5.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+#inisiasi
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+identitas={}
+
+class KResource(Resource):
+    def get(self):
+        return identitas
+    def post(self):
+        name = request.form["name"]
+        keterangan = request.form["keterangan"]
+        identitas["Toto"] = name
+        identitas["Gem"] = name
+        response = ({"msg":"berhasil di post"})
+        return response
+    
+api.add_resource(KResource, "/api",methods =["GET","POST"])
+if __name__ == '__main__':
+    app.run(debug=True,port=5005)
+    
